c9324a812916324f8fe430b21fd8995d1d933729
Maxine Chui <maxine.chui@gmail.com>
2017-10-17 16:57:51 -0700

    Finished QuestionFollow class

--- questions.rb
+++ questions.rb
@@ -81,14 +81,12 @@ class User
     Reply.find_by_user_id(@id)
   end
 
+  def followed_questions
+    QuestionFollows.followed_questions_for_user_id(@id)
+  end
+
 end
-# CREATE TABLE questions (
-#   id INTEGER PRIMARY KEY,
-#   title TEXT NOT NULL,
-#   body TEXT NOT NULL,
-#   users_id INTEGER NOT NULL,
-#
-#   FOREIGN KEY (users_id) REFERENCES users(id)
+
 class Question
   attr_reader :id, :title
 
@@ -148,7 +146,9 @@ class Question
     SQL
     Question.new(quest.first)
   end
-
+  def self.most_followed(n)
+    QuestionFollows.most_followed_questions(n)
+  end
   def initialize(options)
     @title=options['title']
     @body=options['body']
@@ -180,16 +180,18 @@ class Question
     SQL
   end
 
+  def followers
+    QuestionFollows.followers_for_question_id(@id)
+  end
+
 end
-# CREATE TABLE question_follows (
-#   id INTEGER PRIMARY KEY,
-#   users_id INTEGER NOT NULL,
-#   questions_id INTEGER NOT NULL,
+
 class QuestionFollows
   def self.all
     question_all = QuestionsDatabase.instance.execute("SELECT * FROM question_follows")
     question_all.map{|data| QuestionFollows.new(data)}
   end
+
   def self.find_by_id(id)
     quest = QuestionsDatabase.instance.execute(<<-SQL, id)
       SELECT
@@ -202,11 +204,41 @@ class QuestionFollows
     QuestionFollows.new(quest.first)
 
   end
+
+  def self.followers_for_question_id(questions_id)
+    user_ids = QuestionsDatabase.instance.execute(<<-SQL, questions_id)
+      SELECT
+        *
+      FROM
+        users
+      JOIN
+        question_follows on users.id=question_follows.users_id
+      WHERE
+        questions_id=?
+    SQL
+    user_ids.map { |user| User.new(user) }
+  end
+
+  def self.followed_questions_for_user_id(user_id)
+    q_ids = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+      SELECT
+        *
+      FROM
+        questions
+      JOIN
+        question_follows on questions.id = question_follows.questions_id
+      WHERE
+        question_follows.users_id = ?
+    SQL
+    q_ids.map { |q| Question.new(q) }
+  end
+
   def initialize(options)
     @users_id=options['users_id']
     @questions_id=options['questions_id']
     @id=options['id']
   end
+
   def create
     raise "#{self} already in database" if @id
     PlayDBConnection.instance.execute(<<-SQL, @users_id, @questions_id)
@@ -217,6 +249,7 @@ class QuestionFollows
     SQL
     @id = QuestionsDatabase.instance.last_insert_row_id
   end
+
   def update
     raise "#{self} not in database" unless @id
     QuestionsDatabase.instance.execute(<<-SQL, @users_id, @questions_id, @id)
@@ -227,6 +260,20 @@ class QuestionFollows
       WHERE
         id = ?
     SQL
+  end
+
+  def self.most_followed_questions(n)
+    qs=QuestionsDatabase.instance.execute(<<-SQL,n)
+
+      SELECT *
+      from questions
+      JOIN question_follows on question_follows.questions_id=questions.id
+      GROUP BY questions_id
+      order by count(question_follows.users_id)
+      Limit ?
+    SQL
+    qs.map{|quest| Question.new(quest)}
+
   end
 
 end
@@ -339,6 +386,61 @@ class Reply
 
 end
 
-class QuestionLikes
+class QuestionLike
+
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM question_likes")
+    data.map { |datum| QuestionLike.new(datum) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @users_id = options['users_id']
+    @questions_id = options['questions_id']
+  end
+
+  def create
+    raise "#{self} already in the database" if @id
+    QuestionsDatabase.instance.execute(<<-SQL, @users_id, @questions_id)
+      INSERT INTO
+        question_likes(users_id, questions_id)
+      VALUES
+        (?,?)
+    SQL
+    @id = QuestionsDatabase.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in databse " unless @id
+    QuestionsDatabase.instance.execute(<<-SQL, @users_id, @questions_id, @id)
+      UPDATE
+        question_likes
+      SET
+        users_id=?, questions_id=?
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def self.likers_for_question_id(question_id)
+    likers = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+    SELECT
+      *
+    FROM
+      users
+    JOIN
+      question_likes on question_likes.users_id = users.id
+    WHERE
+      questions_id = ?
+    SQL
+    likers.map {|liker| User.new(liker)}
+  end
+
+  def self.num_likes_for_quesiton_id(question_id)
+
+  end
+
+  def self.liked_questions_for_user_id(user_id)
+  end
 
 end
